3eb6624e0fa037e64619cb0224d2d0a2542bd54a
Roman Dolgyi <tosters@gmail.com>
2020-02-19 01:54:32 -0600

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,48 +1,54 @@
 const {events, Job, Group} = require("brigadier");
-const checkRunImage = "brigadecore/brigade-github-check-run:latest"
+const checkRunImage = "brigadecore/brigade-github-check-run:latest";
 
-events.on("check_suite:requested", checkRequested)
-events.on("check_suite:rerequested", checkRequested)
-events.on("check_run:rerequested", checkRequested)
+events.on("check_suite:requested", checkRequested);
+events.on("check_suite:rerequested", checkRequested);
+events.on("check_run:rerequested", checkRequested);
 
 async function checkRequested(e, p) {
-  console.log("check requested")
+  console.log("check requested");
+  console.log(e);
+  console.log(p);
   async function _checkRequested(name, n) {
     // Common configuration
     const env = {
       CHECK_PAYLOAD: e.payload,
       CHECK_NAME: name,
       CHECK_TITLE: "Echo Test",
-    }
+    };
 
     // This will represent our build job. For us, it's just an empty thinger.
-    const build = new Job(`build-${n}`, "alpine:3.7", ["sleep 2", "echo hello"])
+    const build = new Job(`build-${n}`, "alpine:3.7", ["sleep 2", "echo hello"]);
 
     // For convenience, we'll create three jobs: one for each GitHub Check
     // stage.
-    const start = new Job(`start-run-${n}`, checkRunImage)
+    const start = new Job(`start-run-${n}`, checkRunImage);
     // start.imageForcePull = true
-    start.env = env
-    start.env.CHECK_SUMMARY = "Beginning test run"
-    start.env.CHECK_DETAILS_URL = "https://google.com"
+    start.env = env;
+    start.env.CHECK_SUMMARY = "Beginning test run";
+    start.env.CHECK_DETAILS_URL = "https://google.com";
 
-    const end = new Job(`end-run-${n}`, checkRunImage)
+    const end = new Job(`end-run-${n}`, checkRunImage);
     // end.imageForcePull = true
-    end.env = env
+    end.env = env;
 
-    start.run()
+    start.run();
     try {
-      result = await build.run()
-      end.env.CHECK_CONCLUSION = "success"
-      end.env.CHECK_SUMMARY = "Build completed"
-      end.env.CHECK_TEXT = result.toString()
-      end.env.CHECK_DETAILS_URL = "https://google.com"
-      await end.run()
+      result = await build.run();
+      end.env.CHECK_CONCLUSION = "success";
+      end.env.CHECK_SUMMARY = "Build completed";
+      end.env.CHECK_TEXT = `
+        ${result.toString()}
+        ${JSON.stringify(e)}
+        ${JSON.stringify(p)}
+      `;
+      end.env.CHECK_DETAILS_URL = "https://google.com";
+      await end.run();
     } catch (err) {
-      end.env.CHECK_CONCLUSION = "failure"
-      end.env.CHECK_SUMMARY = "Build failed"
-      end.env.CHECK_TEXT = `Error: ${ err }`
-      await end.run()
+      end.env.CHECK_CONCLUSION = "failure";
+      end.env.CHECK_SUMMARY = "Build failed";
+      end.env.CHECK_TEXT = `Error: ${ err }`;
+      await end.run();
     }
   }
 
